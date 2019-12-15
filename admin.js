b856af77ffc1b6262d0b628e076f4f159d2f673b
VladTvardovskyi <45090420+VladTvardovskyi@users.noreply.github.com>
2019-12-15 12:47:43 +0200

    Lab_8
    Creating local storage

--- admin.js
+++ admin.js
@@ -1,66 +1,85 @@
-// const img = document.getElementById("img");
-// console.log(img.src);
 
+const realFileBtn = document.getElementById("real-file");
+const customBtn = document.getElementById("custom-button");
+const img = document.getElementById("img");
 
-// customBtn.addEventListener("click", function() {
-//   realFileBtn.click();
-// });
+customBtn.addEventListener("click", function() {
+    realFileBtn.click();
+  });
+  
+  realFileBtn.addEventListener("change", function() {
+    if (realFileBtn.value) {
+      console.log(URL.createObjectURL(this.files[0]));
+      document.getElementById("img").src = URL.createObjectURL(this.files[0]);
+      console.log(img.src);
+      
+    } 
+  });
+  
+  document.getElementById("send").addEventListener("click", sendNews);
+//   window.addEventListener("online", function (event) {
+//       const allNews = readNewsFromLocalStorage();
+//       sendNewsToServer(allNews);
+//       showAllNews(allNews);
+//       localStorage.removeItem("news");
+//   });
 
-// realFileBtn.addEventListener("change", function() {
-//   if (realFileBtn.value) {
-//     console.log(URL.createObjectURL(this.files[0]));
-//     document.getElementById("img").src = URL.createObjectURL(this.files[0]);
-//     console.log(img.src);
-// } 
-// });
+//   const allNews = readNewsFromLocalStorage();
+//   if(isOnline()){
+//       sendNewsToServer(allNews);
+//       showAllNews(allNews);
+//       localStorage.removeItem("news");
+//   }
+// function verifyInput() {
+//     var txtVal1 = document.getElementById('newsTitle');
+//     var txtVal2 = document.getElementById('newsBody');
 
+//     if (txtVal1.value.trim() === '') {
+//         return false;
+//     } else if (txtVal2.value.trim() === '') {
+//         return false;
+//     } else {
+//         return true;
+//     }
+// }
 
-function verifyInput() {
-    var titleField = document.getElementById('newsTitle');
-    var bodyField = document.getElementById('newsBody');
-
-    if (titleField.value.trim() === '') {
-        return false;
-    } else if (bodyField.value.trim() === '') {
-        return false;
-    } else {
-        return true;
-    }
-}
-var control = document.getElementById("your-files");
-control.addEventListener("change", function(event) {
-    // Когда происходит изменение элементов управления, значит появились новые файлы
-    var i = 0,
-        files = control.files,
-        len = files.length;
- 
-    for (; i < len; i++) {
-        console.log("Filename: " + files[i].name);
-        console.log("Type: " + files[i].type);
-        console.log("Size: " + files[i].size + " bytes");
-    }
- 
-}, false);
-
-function new_news() {
-    if (verifyInput()) {
-        alert('News was sent');
-        document.getElementById('newsTitle').value = "";
-        document.getElementById('newsBody').value = "";
-    } else if(( document.getElementById('newsTitle').value = "")|| document.getElementById('newsBody')) {
+function sendNews() {     
+    var txtVal1=document.getElementById('newsTitle').value;
+    var txtVal2=document.getElementById('newsBody').value;
+     if((!/\S/.test(txtVal1)) || (!/\S/.test(txtVal2))) {
         alert('Edit all fields please');
-    }else{
-        alert('Write info');
     }
-    // if (img.src==="file:///F:/WEB/No-FuckUP/gettyimages-686157014-594x594.jpg") {
-    //     alert("картинка не заповнене")
-    // }
-    // else{
-
-    //   alert("новина додана ");
+    else{
+            
+    if (isOnline()) {
+        addNews(imgSrc, title, body);
+    } else {
+            let allNews = readNewsFromLocalStorage();
+        allNews.push({imgSrc: img.src, title: txtVal1, body: txtVal2});
+        saveNewsToLocalStorage(allNews);
+        alert("Saved to local storage");
+    }
     //   document.getElementById('form').reset();
-    //   img.src="file://F:WEB/No-FuckUP/martin-garrix.jpg";
-    // }
+     img.src="gettyimages-686157014-594x594.jpg";
 
+    } 
+    function sendNewsToServer(allNews) {
+        if (allNews.length) {
+            alert("Successfully sent to server!")
+        }
+    }
+    
+    function saveNewsToLocalStorage(allNews) {
+        localStorage.setItem("news", JSON.stringify(allNews));
+    }
+    
+    function readNewsFromLocalStorage() {
+        return JSON.parse(localStorage.getItem("news")) != null
+            ? JSON.parse(localStorage.getItem("news")) : [];
+    }
+    
+    function isOnline() {
+        return window.navigator.onLine;
+    }
 
-};
+}
